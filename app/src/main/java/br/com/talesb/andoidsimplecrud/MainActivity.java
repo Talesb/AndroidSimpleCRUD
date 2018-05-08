6eb4387f450d8cb05229d9172c0c7477d5a62867
Talesb <talesbatistafig@hotmail.com>
2018-05-08 13:49:30 -0300

    implementing MainActivity and activity_main

--- app/src/main/java/br/com/talesb/andoidsimplecrud/MainActivity.java
+++ app/src/main/java/br/com/talesb/andoidsimplecrud/MainActivity.java
@@ -2,6 +2,7 @@ package br.com.talesb.andoidsimplecrud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void create(View view) {
+    }
+
+    public void update(View view) {
+    }
+
+    public void delete(View view) {
+    }
+
+    public void find(View view) {
+    }
+
+    public void quit(View view) {
+        finish();
     }
 }
